71fefffe613cf545730442dbee966a33ccbd5885
David Caldera <davidcaldera13@yahoo.com>
2022-03-02 12:03:02 -0600

    commit tree a19d6e8b7a6985b712d9efdfbcdbeb4b3d25809c

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const bookData = fetch('https://anapioficeandfire.com/api/books').then(books => books.json()).then(books => renderBooks(books))
+  return bookData
 }
 
 function renderBooks(books) {
